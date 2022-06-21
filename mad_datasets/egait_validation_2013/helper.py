9a08810be16d22a3ec99e9f7ac6d259dd3dc7498
Arne Küderle <a.kuederle@gmail.com>
2022-06-21 12:31:58 +0200

    Added calibration

--- mad_datasets/egait_validation_2013/helper.py
+++ mad_datasets/egait_validation_2013/helper.py
@@ -4,6 +4,7 @@ from typing import Dict, Literal
 
 import numpy as np
 import pandas as pd
+from imucal import FerrarisCalibrationInfo
 
 from mad_datasets.utils.data_loading import load_bin_file
 
@@ -25,8 +26,13 @@ SHIMMER2_DATA_LAYOUT = {
     "gyr_z": np.uint16,
 }
 
+CALIBRATION_FILE_NAMES = {
+    "left_sensor": "A917.csv",
+    "right_sensor": "A6DF.csv",
+}
 
-def rename_sh2_axes(
+
+def transform_shimmer2_axes(
     dataset: Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame]
 ) -> Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame]:
     """Transform shimmer2 axes to align acc and gyroscope.
@@ -58,8 +64,26 @@ def rename_sh2_axes(
     return dataset
 
 
+def calibrate_shimmer2_data(
+    data: Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame],
+    calibration_base_path: Path,
+    calibration_mapping: Dict[Literal["left_sensor", "right_sensor"], str],
+) -> Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame]:
+    """Calibrate shimmer2 data.
+    """
+    for sensor in ["left_sensor", "right_sensor"]:
+        cal_path = calibration_base_path / calibration_mapping[sensor]
+        cal_matrix = load_compact_cal_matrix(cal_path)
+        data[sensor] = cal_matrix.calibrate_df(data[sensor], "a.u.", "a.u.")
+
+    return data
+
+
 def load_shimmer2_data(
-    left_sensor_path: Path, right_sensor_path: Path
+    left_sensor_path: Path,
+    right_sensor_path: Path,
+    calibration_base_path: Path,
+    calibration_mapping: Dict[Literal["left_sensor", "right_sensor"], str],
 ) -> Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame]:
     """Load shimmer2 data from a file."""
 
@@ -67,5 +91,24 @@ def load_shimmer2_data(
         "left_sensor": load_bin_file(left_sensor_path, SHIMMER2_DATA_LAYOUT),
         "right_sensor": load_bin_file(right_sensor_path, SHIMMER2_DATA_LAYOUT),
     }
-    data = rename_sh2_axes(data)
+    data = calibrate_shimmer2_data(data, calibration_base_path, calibration_mapping)
+    data = transform_shimmer2_axes(data)
     return data
+
+
+def load_compact_cal_matrix(path: Path) -> FerrarisCalibrationInfo:
+    """Load a compact calibration matrix from a file."""
+    cal_matrix = np.genfromtxt(path, delimiter=",")
+    plus_g = cal_matrix[0]
+    minus_g = cal_matrix[1]
+    b_a = (plus_g + minus_g) / 2
+    K_a = np.eye(3) * (plus_g - minus_g) / 2
+    R_a = np.eye(3)
+    b_g = cal_matrix[2]
+    K_g = np.eye(3) * 2.731
+    R_g = np.eye(3)
+    K_ga = np.zeros((3, 3))
+
+    return FerrarisCalibrationInfo(
+        b_a=b_a, K_a=K_a, R_a=R_a, b_g=b_g, K_g=K_g, R_g=R_g, K_ga=K_ga, from_acc_unit="a.u.", from_gyr_unit="a.u."
+    )
--- tests/test_egait_validation_2013.py
+++ tests/test_egait_validation_2013.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 import pandas as pd
-from pandas._testing import assert_frame_equal
+from pandas._testing import assert_frame_equal, assert_series_equal
 
-from mad_datasets.egait_validation_2013.helper import load_shimmer2_data, SHIMMER2_DATA_LAYOUT
+from mad_datasets.egait_validation_2013.helper import load_shimmer2_data, SHIMMER2_DATA_LAYOUT, load_compact_cal_matrix, \
+    CALIBRATION_FILE_NAMES
+from mad_datasets.utils.consts import SF_ACC
 from mad_datasets.utils.data_loading import load_bin_file
 
 base_dir = Path("/home/arne/Documents/repos/work/datasets/eGaIT_database")
@@ -16,16 +18,27 @@ def test_basic_data_loading():
     assert_frame_equal(data.astype(int), reference_data.drop(columns=["n_samples"]))
 
 
+def test_calibration():
+    data = load_bin_file(Path("./egait_validation_2013_test_data/P115_E4_left.dat"), SHIMMER2_DATA_LAYOUT).astype(float)
+    cal_matrix = load_compact_cal_matrix(Path("./egait_validation_2013_test_data/A917.csv"))
+    calibrated_data = cal_matrix.calibrate_df(data, "a.u.", "a.u.")
+    # Reference data produced by the matlab import script
+    reference_data = pd.read_csv("./egait_validation_2013_test_data/P115_E4_left_calibrated.csv", header=0)
+    assert_frame_equal(calibrated_data, reference_data.drop(columns=["n_samples"]))
+
+
 def test_data_loading_with_transformation():
     data = load_shimmer2_data(
         Path("./egait_validation_2013_test_data/P115_E4_left.dat"),
         Path("./egait_validation_2013_test_data/P115_E4_right.dat"),
+        Path("./egait_validation_2013_test_data"),
+        CALIBRATION_FILE_NAMES
     )
     reference_data = {
-        "left_sensor": pd.read_csv("./egait_validation_2013_test_data/P115_E4_left.csv", header=0).drop(
+        "left_sensor": pd.read_csv("./egait_validation_2013_test_data/P115_E4_left_calibrated.csv", header=0).drop(
             columns=["n_samples"]
         ),
-        "right_sensor": pd.read_csv("./egait_validation_2013_test_data/P115_E4_right.csv", header=0).drop(
+        "right_sensor": pd.read_csv("./egait_validation_2013_test_data/P115_E4_right_calibrated.csv", header=0).drop(
             columns=["n_samples"]
         ),
     }
@@ -36,7 +49,7 @@ def test_data_loading_with_transformation():
         reference_data[sensor] = reference_data[sensor].astype(float)
 
         assert data[sensor].shape == reference_data[sensor].shape
-        assert data[sensor]["gyr_x"].equals(reference_data[sensor]["gyr_y"])
-        assert data[sensor]["gyr_y"].equals(reference_data[sensor]["gyr_x"])
-        assert data[sensor]["gyr_z"].equals(-reference_data[sensor]["gyr_z"])
-
+        assert_series_equal(data[sensor]["gyr_x"], reference_data[sensor]["gyr_y"], check_names=False)
+        assert_series_equal(data[sensor]["gyr_y"], reference_data[sensor]["gyr_x"], check_names=False)
+        assert_series_equal(data[sensor]["gyr_z"], -reference_data[sensor]["gyr_z"], check_names=False)
+        assert_frame_equal(data[sensor][SF_ACC], reference_data[sensor][SF_ACC])
