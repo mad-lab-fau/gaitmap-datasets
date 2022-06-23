3c0e2792e1806e7c3573f90a7d9127572143b4e0
Arne Küderle <a.kuederle@gmail.com>
2022-06-23 13:46:02 +0200

    Implemented data loading

--- mad_datasets/egait_segmentation_validation_2013/helper.py
+++ mad_datasets/egait_segmentation_validation_2013/helper.py
@@ -3,13 +3,26 @@
 These are the logic behind the dataset implementation, but can also be used independently.
 """
 from pathlib import Path
-from typing import Literal, Optional, List, Tuple
+from typing import Literal, Optional, List, Tuple, Dict
+
+import pandas as pd
+from scipy.spatial.transform import Rotation
+
+from mad_datasets.utils.coordinate_transforms import rotate_sensor
+from mad_datasets.utils.egait_loading_helper import load_shimmer2_data
 
 Cohorts = Literal["control", "pd", "geriatric"]
 Tests = Literal["4x10m", "free_walk"]
 
 _test_rename_dict = {"4x10m": "", "free_walk": "_4MW"}
 _cohort_rename_dict = {"control": "Control", "pd": "PD", "geriatric": "Geriatric"}
+
+COORDINATE_SYSTEM_TRANSFORMATION = {  # egait_lateral_shimmer2r
+    # [[-y -> +x], [+z -> +y], [-x -> +z]]
+    "left_sensor": [[0, -1, 0], [0, 0, 1], [-1, 0, 0]],
+    # [[+y -> +x], [-z -> +y], [-x -> +z]]
+    "right_sensor": [[0, 1, 0], [0, 0, -1], [-1, 0, 0]],
+}
 
 
 def _raw_data_folder(base_dir: Path, cohort: Cohorts, test: Tests) -> Path:
@@ -20,6 +33,18 @@ def _raw_data_folder(base_dir: Path, cohort: Cohorts, test: Tests) -> Path:
 def _reference_stride_borders_folder(base_dir: Path, cohort: Cohorts, test: Tests) -> Path:
     """Return the relative path to the reference stride borders subfolder."""
     return base_dir / f"{_cohort_rename_dict[cohort]}s_GoldStandard_StrideBorders{_test_rename_dict[test]}"
+
+
+def _calibration_folder(base_dir: Path) -> Path:
+    """Return the relative path to the imu-calibration subfolder."""
+    return base_dir / "calibrationFiles"
+
+
+def _get_sensor_node(stride_segmentation_file_path: Path) -> str:
+    """Get the sensor node id by reading the respective entry in the file."""
+    # The information is stored in the 4th line of the file.
+    with stride_segmentation_file_path.open() as f:
+        return f.readlines()[3].split(",")[1].strip().upper()
 
 
 def _extract_participant_id(file_name: str, test: Tests) -> str:
@@ -33,6 +58,25 @@ def _extract_participant_id(file_name: str, test: Tests) -> str:
     raise ValueError("Invalid file format")
 
 
+def find_files_for_participant(
+    directory: Path, participant_id: str
+) -> Dict[Literal["left_sensor", "right_sensor"], Optional[Path]]:
+    """Find the file for a participant."""
+    participant_id = participant_id.lower()
+    files = {"left_sensor": None, "right_sensor": None}
+    for file in directory.iterdir():
+        if file.is_file():
+            file_name = file.name.lower()
+            if participant_id in file_name:
+                if "left" in file_name:
+                    files["left_sensor"] = file
+                elif "right" in file_name:
+                    files["right_sensor"] = file
+        if all(files.values()):
+            break
+    return files
+
+
 def get_all_participants(*, base_dir: Optional[Path] = None) -> List[Tuple[str, str, str]]:
     """Get the folder names of all participants."""
     all_participants = []
@@ -44,3 +88,26 @@ def get_all_participants(*, base_dir: Optional[Path] = None) -> List[Tuple[str, 
                     participant_id = _extract_participant_id(file_name.name, test)
                     all_participants.append((cohort, test, participant_id))
     return list(set(all_participants))
+
+
+def get_all_data_for_participant(
+    participant_id: str, cohort: Cohorts, test: Tests, *, base_dir: Optional[Path] = None
+) -> Dict[Literal["left_sensor", "right_sensor"], pd.DataFrame]:
+    """Get the data for a participant."""
+    raw_data_folder = _raw_data_folder(base_dir, cohort, test)
+    files = find_files_for_participant(raw_data_folder, participant_id)
+    # We need to load the stride segmentation data as well, because it contains the information about the calibration
+    # files.
+    stride_segmentation_folder = _reference_stride_borders_folder(base_dir, cohort, test)
+
+    all_data = {}
+    for sensor, file_path in files.items():
+        if file_path is None:
+            all_data[sensor] = None
+            continue
+        stride_segmentation_file = stride_segmentation_folder / file_path.with_suffix(".txt").name
+        calibration_file_path = _calibration_folder(base_dir) / f"{_get_sensor_node(stride_segmentation_file)}.csv"
+        data = load_shimmer2_data(file_path, calibration_file_path)
+        data = rotate_sensor(data, Rotation.from_matrix(COORDINATE_SYSTEM_TRANSFORMATION[sensor]))
+        all_data[sensor] = data
+    return all_data
--- tests/test_egait_segmentation_validation_2013.py
+++ tests/test_egait_segmentation_validation_2013.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
-from mad_datasets.egait_segmentation_validation_2013.helper import get_all_participants
+from mad_datasets.egait_segmentation_validation_2013.helper import get_all_participants, get_all_data_for_participant
+from mad_datasets.utils.consts import SF_COLS
 
 base_dir = Path("/home/arne/Documents/repos/work/datasets/eGaIT_database_segmentation")
 
@@ -13,3 +14,16 @@ def test_get_all_participants():
     assert set(cohorts) == {"control", "pd", "geriatric"}
     assert set(tests) == {"4x10m", "free_walk"}
     assert len(participants) == 45
+
+
+def test_get_data():
+    # We test loading for **all** participants, because there are too many werid things that we need to check for
+    for cohort, test, participant_id in get_all_participants(base_dir=base_dir):
+        data = get_all_data_for_participant(participant_id, cohort, test, base_dir=base_dir)
+        assert list(data.keys()) == ["left_sensor", "right_sensor"]
+        for sensor in ["left_sensor", "right_sensor"]:
+            if sensor == "right_sensor" and (cohort, test, participant_id) == ("control", "free_walk", "GA214026"):
+                assert data[sensor] is None
+            else:
+                assert data[sensor].shape[0] > 0
+                assert data[sensor].columns.tolist() == SF_COLS
