7561ca0881c98213a9c44982a0f487030636c0a7
Arne Küderle <a.kuederle@gmail.com>
2022-12-21 10:22:36 +0100

    Implemented dataset for the stair portion

--- examples/pyshoe_2019.py
+++ examples/pyshoe_2019.py
@@ -11,6 +11,7 @@ The data is split into three parts:
 - Vicon: Individual trials recorded with a Vicon motion capture system as reference
 - Hallway: Multiple longer walking/running trials recorded with specific landmarks as positional reference along the
            trial
+- Stairs: Multiple trials from a single participant recorded with a stair climbing task
 
 For each part of the data we provide a separate dataset class.
 """
@@ -105,6 +106,50 @@ reference
 # Hence, we can easily get the IMU data for the time points of the reference (or the position, once we have calculated
 # it based on the IMU data).
 imu_data.loc[reference.index]
+
+# %%
+# Below we plot all the data together.
+
+fig, axs = plt.subplots(3, 1, sharex=True)
+imu_data["right_sensor"].filter(like="acc").plot(ax=axs[0])
+imu_data["right_sensor"].filter(like="gyr").plot(ax=axs[1])
+reference["right_sensor"].plot(ax=axs[2], style="o")
+axs[2].set_ylabel("Position [mm]")
+axs[1].set_ylabel("Gyro [deg/s]")
+axs[0].set_ylabel("Acc [m/s^2]")
+
+fig.tight_layout()
+fig.show()
+
+# %%
+# Stairs Dataset
+# --------------
+# First we will create a simple instance of the dataset class.
+from gaitmap_datasets.pyshoe_2019 import PyShoe2019Stairs
+
+dataset = PyShoe2019Stairs(data_folder=dataset_path)
+dataset
+
+# %%
+# We can simply select either the up or down trials.
+subset = dataset.get_subset(direction="up")
+subset
+
+# %%
+# When we have selected a single trial, we can access the data.
+trial = subset[0]
+trial
+
+# %%
+# We can access the IMU data as before.
+imu_data = trial.data
+imu_data
+
+# %%
+# As with the hallway dataset the reference position is only provided for individual points along the trial.
+# Further, as the reference is derived from the stair geometry, reference only exist for the z-axis.
+reference = trial.position_reference_
+reference
 
 # %%
 # Below we plot all the data together.
--- gaitmap_datasets/pyshoe_2019/_dataset.py
+++ gaitmap_datasets/pyshoe_2019/_dataset.py
@@ -7,8 +7,10 @@ from tpcp import Dataset
 
 from gaitmap_datasets.pyshoe_2019.helper import (
     get_all_hallway_trials,
+    get_all_stairs_trials,
     get_all_vicon_trials,
     get_data_hallway,
+    get_data_stairs,
     get_data_vicon,
 )
 
@@ -144,6 +146,94 @@ class PyShoe2019Hallway(Dataset):
         trial = self.group
         return get_data_hallway(trial, base_dir=self._data_folder_path)[1]
 
+    @property
+    def position_reference_index_(self) -> pd.Series:
+        """Get the indices when the position reference was sampled."""
+        self.assert_is_single(None, "position_reference_index_")
+        trial = self.group
+        return get_data_hallway(trial, base_dir=self._data_folder_path)[2]
+
     def create_index(self) -> pd.DataFrame:
         """Create the index for the dataset."""
         return pd.DataFrame(get_all_hallway_trials(self._data_folder_path), columns=["participant", "type", "trial"])
+
+
+class PyShoe2019Stairs(Dataset):
+    """Dataset helper for the staircase portion for the PyShoe dataset.
+
+    Note, this only contains the data of the "test" subfolder, as only this part of the data contains the ground truth
+    reference derived based on the stair geometries.
+
+    Parameters
+    ----------
+    data_folder
+        The base folder where the dataset can be found.
+        Note, this should be the folder that was created when downloading the PyShoe dataset and *not* just the
+        "data" sub-folder.
+    groupby_cols
+        `tpcp` internal parameters.
+    subset_index
+        `tpcp` internal parameters.
+
+    """
+
+    data_folder: Union[str, Path]
+
+    def __init__(
+        self,
+        data_folder: Union[str, Path],
+        *,
+        groupby_cols: Optional[Union[List[str], str]] = None,
+        subset_index: Optional[pd.DataFrame] = None
+    ):
+        self.data_folder = data_folder
+        super().__init__(groupby_cols=groupby_cols, subset_index=subset_index)
+
+    @property
+    def sampling_rate_hz(self) -> float:
+        """Get the sampling rate of the IMUs."""
+        return 200.0
+
+    @property
+    def _data_folder_path(self) -> Path:
+        """Get the path to the data folder as Path object."""
+        return Path(self.data_folder)
+
+    @property
+    def data(self) -> pd.DataFrame:
+        """Get the imu data.
+
+        The index is provided as seconds since the start of the trial.
+        """
+        self.assert_is_single(None, "data")
+        trial = self.group
+        return get_data_stairs(trial, base_dir=self._data_folder_path)[0]
+
+    @property
+    def position_reference_(self) -> pd.DataFrame:
+        """Get the position reference along the trial in mm from the starting position.
+
+        The returning dataframe provides the expected position of the sensor during specific time points during the
+        trials.
+        The index is provided as seconds since the start of the trial and should line up with the imu data.
+
+        If the sampling point of the reference is required as indices of the imu data, use the
+        `position_reference_index_` property.
+
+        Note, that for this dataset only reference for the z-level is provided as the ground truth was derived based on
+        the stair geometries.
+        """
+        self.assert_is_single(None, "position_reference_")
+        trial = self.group
+        return get_data_stairs(trial, base_dir=self._data_folder_path)[1]
+
+    @property
+    def position_reference_index_(self) -> pd.Series:
+        """Get the indices when the position reference was sampled."""
+        self.assert_is_single(None, "position_reference_index_")
+        trial = self.group
+        return get_data_stairs(trial, base_dir=self._data_folder_path)[2]
+
+    def create_index(self) -> pd.DataFrame:
+        """Create the index for the dataset."""
+        return pd.DataFrame(get_all_stairs_trials(self._data_folder_path), columns=["direction", "trial"])
